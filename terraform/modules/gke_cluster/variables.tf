9cafaa6de3bbc1c4d84958a735ecb99d1ba09ec5
PranavKhurud23 <156734217+PranavKhurud23@users.noreply.github.com>
2026-09-20 15:48:26 +0530

    Add variables for GKE cluster configuration

---
+++ terraform/modules/gke_cluster/variables.tf
@@ -0,0 +1,42 @@
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+  type        = string
+  default     = "devops-gke-cluster"
+}
+
+variable "region" {
+  description = "The GCP region for the cluster"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC network"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnetwork"
+  type        = string
+}
+
+variable "pod_range_name" {
+  description = "The secondary range name for pods"
+  type        = string
+}
+
+variable "svc_range_name" {
+  description = "The secondary range name for services"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Initial node count per zone"
+  type        = number
+  default     = 1
+}
+
+variable "machine_type" {
+  description = "Machine type for cluster nodes"
+  type        = string
+  default     = "e2-medium"
+}
