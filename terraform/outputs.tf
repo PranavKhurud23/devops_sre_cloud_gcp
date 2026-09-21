df6c94150e5ffc11f194fc0b95e1544c8b515a5b
PranavKhurud23 <156734217+PranavKhurud23@users.noreply.github.com>
2026-09-21 22:33:13 +0530

    Add GKE cluster endpoint output and update VPC output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,11 +2,13 @@ output "gke_cluster_name" {
   value       = module.gke_cluster.kubernetes_cluster_name
   description = "The name of the GKE cluster"
 }
+
 output "gke_cluster_endpoint" {
   value       = module.gke_cluster.kubernetes_cluster_host
   description = "The endpoint for the GKE cluster"
 }
-output "vpc_name" {
-  value       = module.vpc.vpc_name
-  description = "The name of the created VPC"
+
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "The ID of the created VPC"
 }
