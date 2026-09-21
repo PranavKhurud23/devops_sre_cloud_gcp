c3cb84d114914c74ccc90e578fb2ff33b6232bde
PranavKhurud23 <pranav.khurud230896@gmail.com>
2026-09-21 13:52:36 +0000

    Format Fix

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -15,8 +15,8 @@ resource "google_compute_subnetwork" "custom_subnet" {
   }
 
   secondary_ip_range {
-    range_name    = "k8s-service-range"
+    range_name = "k8s-service-range"
     #ip_cidr_range = var.services_cidr
-     ip_cidr_range = var.service_cidr
+    ip_cidr_range = var.service_cidr
   }
 }
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,20 +1,20 @@
 variable "vpc_name" {
-  type = string
+  type        = string
   description = "Name of the Custom VPC netwrok"
 }
 variable "subnet_cidr" {
-  type = string
-  description = "Primary UP CIDR range of subnetwork" 
+  type        = string
+  description = "Primary UP CIDR range of subnetwork"
 }
 variable "pods_cidr" {
-  type = string
+  type        = string
   description = "Secondary IP range of GKR PODS"
 }
-variable "service_cidr"{
-  type = string
+variable "service_cidr" {
+  type        = string
   description = "Secndary IP CIDR range for GKR service"
 }
 variable "region" {
-  type = string
+  type        = string
   description = "GCP region for subnet"
 }
