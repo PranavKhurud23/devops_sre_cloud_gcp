a8883dd93a5c25469aff20982d947cfe7e6e9458
PranavKhurud23 <156734217+PranavKhurud23@users.noreply.github.com>
2026-09-21 22:34:01 +0530

    Update VPC outputs with descriptions and rename fields

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,24 +1,29 @@
 output "vpc_id" {
-  description = "The ID of the created VPC network"
   value       = google_compute_network.custom_vpc.id
+  description = "The URI / ID of the created custom VPC"
+}
+
+output "vpc_name" {
+  value       = google_compute_network.custom_vpc.name
+  description = "The exact name of the created VPC"
 }
 
 output "subnet_id" {
-  description = "The ID of the created subnetwork"
   value       = google_compute_subnetwork.custom_subnet.id
+  description = "The URI / ID of the created subnetwork"
 }
 
 output "subnet_name" {
-  description = "The name of the created subnetwork"
   value       = google_compute_subnetwork.custom_subnet.name
+  description = "The exact name of the subnetwork"
 }
 
-output "pod_range_name" {
-  description = "The secondary range name for pods"
+output "pods_range_name" {
   value       = google_compute_subnetwork.custom_subnet.secondary_ip_range[0].range_name
+  description = "The secondary IP range name used for GKE Pods"
 }
 
-output "svc_range_name" {
-  description = "The secondary range name for services"
+output "services_range_name" {
   value       = google_compute_subnetwork.custom_subnet.secondary_ip_range[1].range_name
+  description = "The secondary IP range name used for GKE Services"
 }
