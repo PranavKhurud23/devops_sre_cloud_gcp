7c5e97fdb66cce848d0b08e649de0b2bfb609a0e
PranavKhurud23 <156734217+PranavKhurud23@users.noreply.github.com>
2026-09-22 20:31:22 +0530

    Update main.tf

--- terraform/modules/gke_cluster/main.tf
+++ terraform/modules/gke_cluster/main.tf
@@ -18,6 +18,7 @@ resource "google_container_cluster" "primary" {
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = false
+    master_ipv4_cidr_block  = var.master_ipv4_cidr_block
   }
 }
 
