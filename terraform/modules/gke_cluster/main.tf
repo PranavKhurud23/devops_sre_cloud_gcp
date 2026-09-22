4868395472114c412eb16080646af996bb353dc6
PranavKhurud23 <156734217+PranavKhurud23@users.noreply.github.com>
2026-09-22 23:17:56 +0530

    Add deletion protection setting to GKE cluster

--- terraform/modules/gke_cluster/main.tf
+++ terraform/modules/gke_cluster/main.tf
@@ -2,6 +2,7 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
+  deletion_protection = false
 
   # Remove default node pool to manage worker nodes separately
   remove_default_node_pool = true
