b2825c3e19fd2730e7630f0e6d1065e1e7d9b5ce
PranavKhurud23 <156734217+PranavKhurud23@users.noreply.github.com>
2026-09-22 13:07:02 +0530

    Update GKE outputs to use new module attributes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
 output "gke_cluster_name" {
-  value       = module.gke_cluster.cluster_name
+  value       = module.gke_cluster.kubernetes_cluster_name
   description = "The name of the GKE cluster"
 }
 
 output "gke_cluster_endpoint" {
-  value       = module.gke_cluster.cluster_endpoint
+  value       = module.gke_cluster.kubernetes_cluster_host
   description = "The endpoint for the GKE cluster"
 }
 
